bb04ec37e257f4b60fec0499b6c6e4dbd53fccac
Aizaz-Ahmed786 <aizazahmed046@gmail.com>
2024-12-10 11:36:18 +0500

    how to upload dart file on git hub account

---
+++ githubcode.dart
@@ -0,0 +1,30 @@
+void main(){
+
+git config --global user.name "Aizaz-Ahmed786"
+git config --global user.email "aizazahmed046@gmail.com"
+
+
+HOW TO Run the code of dart in CMD terminal
+cd desktop
+cd foldername
+dart run filename.dart
+-----------------------------------------------
+goto Visual code Open terminal and Run this commands
+
+
+
+FOR NEW PROJECT
+
+git init
+git add .  |OR| git add filename.dart
+git commit -m "massage"
+git remote add origin ?goto yourgithub acount and copy the link of new repo
+git push -u origin master
+ 
+
+
+FOR UPDATE FILES & FOLDERS
+git add .  |OR| git add filename.dart
+git commit -m "class1 add and class2 add"
+git push
+}
